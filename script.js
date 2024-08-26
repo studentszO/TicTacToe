76d1c7a6ab5cdfe8907bc6412218a7a524592ac1
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-08-26 17:54:50 +0200

    Add function factory boardController to controll the flow of the game, set the turns & play a round

--- script.js
+++ script.js
@@ -10,44 +10,49 @@ function gameBoard() {
     }
 
     const addMarkToCell = (cell, mark) => {
-        isEmptyCell(cell) ? gameboardBox[cell].push(mark) : console.log(`The cell ${cell} is not empty!`);
+        if (isEmptyCell(cell)) {
+            gameboardBox[cell].push(mark);
+            console.log("SUCCESS!");
+            }
+        else {
+            console.log(`The cell ${cell} is not empty!`);
+            return false;
+        }
     }
 
     return { getBoard, addMarkToCell };
 };
-const board = gameBoard();
-
-
-// gameboardBox = [[13, [1, 2, 3]], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]];
-// gameboardBox[1].push("X")
 
 function boardController() {
-    const sayTurn = () => console.log("It's ${setActivePlayer} turn");
 
     const PlayerX = {
         name: "Player X",
         mark: "X",
-        activePlayer: true
     }
     const PlayerO = {
         name: "Player O",
         mark: "O",
-        activePlayer: false
     }
 
-    const setActivePlayer = (player) => {
-        // player.activePlayer === true ? false
-    };
-    // const checkEmptyCell(cell) = {
-    //     if (gameboardBox[cell][3] !== undefined)
-    //         return 0;
-    //     else return 1;
-    // };
-    // const setMarkToCell(cell, player) = {
-    //     checkEmptyCell(cell);
-    //     if (!checkEmptyCell(cell))
-    //         gameboardBox[cell].push(player.mark);
-    //     endTurn();
-    // };
+    let activePlayer = PlayerX;
 
+    const sayTurn = () => console.log(`It's ${activePlayer.name} turn`);
+
+    const setActivePlayer = () => {
+        activePlayer = activePlayer === PlayerX ? PlayerO : PlayerX;
+        sayTurn(activePlayer);
+    };
+
+    const playRound = (cell) => {
+        if (board.addMarkToCell(cell, activePlayer.mark) !== false)
+            setActivePlayer();
+        else {
+            console.log("CHOOSE ANOTHER CELL!!")
+        }
+    }
+
+    return { playRound }
 }
+
+const board = gameBoard();
+const play = boardController().playRound;

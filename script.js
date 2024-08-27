c7fd71f3ecb3d2b6987e6e1386bc0dd5aa34d37e
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-08-27 15:50:58 +0200

    Change the gameboard array to something better which will help for the implementation of the winner function

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function gameBoard() {
-    const gameboardBox = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]];
+    const gameboardBox = [];
+    for (let cells = 0; cells < 9; cells++)
+        gameboardBox.push(null);
 
     const getBoard = () => gameboardBox;
 
     const isEmptyCell = (cell) => {
-        if (gameboardBox[cell].length > 2)
+        if (gameboardBox[cell] !== null)
             return false;
         else return true;
     }
 
     const addMarkToCell = (cell, mark) => {
         if (isEmptyCell(cell)) {
-            gameboardBox[cell].push(mark);
+            gameboardBox[cell] = mark;
             console.log("SUCCESS!");
             }
         else {
@@ -51,8 +53,40 @@ function boardController() {
         }
     }
 
+    const victoryLines = {
+        lineOne: [0, 1, 2],
+        lineTwo: [3, 4, 5],
+        lineThree: [6, 7, 8],
+        // lineFour:
+        // lineFive:
+        // lineSix:
+        // lineSeven:
+    }
+
+
+
+    const isThereAWinner = (player) => {
+        // lines = [1, 2, 3] [4, 5, 6] [7, 8, 9] [1, 4, 7] [2, 5, 8] [3, 6, 9] [1, 5, 9] [3, 5, 7]
+        if ((gameboardBox[1][2] === gameboardBox[2][2] === gameboardBox[3][2])
+         || (gameboardBox[4][2] === gameboardBox[5][2] === gameboardBox[6][2])
+         || (gameboardBox[7][2] === gameboardBox[8][2] === gameboardBox[9][2])
+         || (gameboardBox[1][2] === gameboardBox[4][2] === gameboardBox[7][2])
+         || (gameboardBox[2][2] === gameboardBox[5][2] === gameboardBox[8][2])
+         || (gameboardBox[3][2] === gameboardBox[6][2] === gameboardBox[9][2])
+         || (gameboardBox[1][2] === gameboardBox[5][2] === gameboardBox[9][2])
+         || (gameboardBox[3][2] === gameboardBox[5][2] === gameboardBox[7][2]))
+         console.log()
+    }
+
     return { playRound }
 }
 
 const board = gameBoard();
 const play = boardController().playRound;
+// FOR TESTING PURPOSE
+board.getBoard()
+play(1)
+play(6)
+play(0)
+play(7)
+play(2)
